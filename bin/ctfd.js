ec7f2f08252feb98e3dcc0f03d80f1f6861106f2
RobertLarsen <robert@the-playground.dk>
2012-04-09 14:14:01 +0000

    Implemented more.

--- bin/ctfd.js
+++ bin/ctfd.js
@@ -1,17 +1,24 @@
 var server = require('net').createServer(),
-    ctfd = require('CTFd'),
-    flagReceiver = new ctfd.FlagReceiver(server);
+    repl = require('repl'),
+    ctfd = require('CTFd');
+    daemon = ctfd.createDaemon({
+        port : 6600,
+        teams : [
+            {
+                name : 'Team 1',
+                host : '192.168.0.1'
+            },
+            {
+                name : 'Team 2',
+                host : '192.168.0.2'
+            }
+        ],
+        services : [
+            {
+                name : 'Service 1',
+                manifest : '/home/robert/code/ctfd/services/SpeakersCorner/Manifest.json'
+            }
+        ]
+    });
 
-server.listen(6000);
-
-flagReceiver.on('connection', function(con) {
-    con.on('team', function(team) {
-        console.log("Team: '" + team + "'");
-    });
-    con.on('flag', function(flag) {
-        console.log("Flag: '" + flag + "'");
-    });
-    con.on('close', function() {
-        console.log("Bye bye");
-    });
-});
+repl.start('ctfd> ').context.daemon = daemon;
