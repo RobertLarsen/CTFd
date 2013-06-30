9d8ce5bec1b10e639bf5bf6995aee432466704c1
RobertLarsen <robert@the-playground.dk>
2013-07-01 00:48:27 +0200

    Made the client more generic.

--- html/scores.js
+++ html/scores.js
@@ -1,17 +1,13 @@
 $(function() {
     var ShowData = function(data) {
-        var counter = 0;
-        $.plot(
-            "#steals_per_service",
-            data.steals_per_service.data,
-            data.steals_per_service.options
-        );
-
-        $.plot(
-            "#steals_losses_per_team",
-            data.steals_losses_per_team.data, 
-            data.steals_losses_per_team.options
-        );
+        _.chain(data)
+         .keys()
+         .forEach(function(name) {
+             $.plot('#' + name,
+                    data[name].data,
+                    data[name].options
+             );
+         });
     };
 
     var UpdateGraphs = function() {
