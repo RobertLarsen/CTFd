ea6cce9bf8c68e1325402041d5a9e7a2f60465c4
RobertLarsen <robert@the-playground.dk>
2013-07-04 00:40:35 +0200

    Show a table with the current score.

--- html/scores.js
+++ html/scores.js
@@ -2,26 +2,33 @@ $(function() {
     var lastData = null,
         large = null,
         ShowData = function(data) {
+            var tableData = '';
             lastData = data;
             if (data) {
-                _.chain(data)
+                _.chain(data.graphs)
                  .keys()
                  .forEach(function(name) {
                      $.plot('.' + name,
-                            data[name].data,
-                            data[name].options
+                            data.graphs[name].data,
+                            data.graphs[name].options
                      );
                 });
+
+                _.forEach(data.table, function(entry) {
+                    tableData += '<tr><td>' + $('<div>').text(entry.team).html() + '</td><td>' + entry.score + '</td></tr>';
+                });
+
+                $('.table_scores tbody').html(tableData);
             }
         };
 
-    var UpdateGraphs = function() {
+    var Update = function() {
         $.ajax('/scores.json', {
             cache : false,
             success : ShowData
         });
     };
 
-    //setInterval(UpdateGraphs, 1000);
-    UpdateGraphs();
+    //setInterval(Update, 1000);
+    Update();
 });
